415de124b3901c84beb160d0c39f0a1679ff48df
Crt <crt.rozi@gmail.com>
2024-09-22 12:22:05 -0700

    test file

---
+++ test.py
@@ -0,0 +1,11 @@
+# File for quick testing and sanity checks
+
+import pandas as pd
+
+loadPath = './python/'
+
+df = pd.read_csv(loadPath + 'MRIT1_longitudinal_UDS.csv')
+
+unique_count = df['NACCID'].nunique()
+
+print(unique_count)

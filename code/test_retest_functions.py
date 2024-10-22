50cdddd1d56e3f6cc3252372f46da28262019723
Crt <crt.rozi@gmail.com>
2024-10-22 10:31:21 -0700

    test retest

--- code/test_retest_functions.py
+++ code/test_retest_functions.py
@@ -9,7 +9,8 @@ import re
 import numpy as np
 from dateutil import parser
 from collections import defaultdict
-
+import nibabel as nib
+import matplotlib.pyplot as plt
 
 
 #--------------------------------------------------------------------------------------------------------------#
@@ -103,14 +104,74 @@ def filepaths(fspgr_folder, mprage_folder, patient_scans):
 #--------------------------------------------------------------------------------------------------------------#
 #                     Function that separates first scans from the second scans                                #
 #--------------------------------------------------------------------------------------------------------------#
+
 def separate_scans(files):
     first_scans = []
     second_scans = []
-    for i in range(len(files)):
-        if i % 2 == 0:
+    for i in range(len(files)): 
+        if i % 2 == 0:                     # even index is first scan, odd is second scan
             first_scans.append(files[i])
         else: 
             second_scans.append(files[i])
     return first_scans, second_scans
 
+
+
+
 #--------------------------------------------------------------------------------------------------------------#
+#              Function to prepare patient data: flatten 3D array to 1D and normalize by intensity             #
+#--------------------------------------------------------------------------------------------------------------#
+
+def flatten_and_normalize(nii_files, mask=False):
+    flat_nii_files = []                 # array to store flattened files
+    for nii_file in nii_files:
+        img = nib.load(nii_file) 
+        img_data = img.get_fdata()       # extracting data
+        img_1d = img_data.flatten()      # flattening array
+        flat_nii_files.append(img_1d)
+    # transform into numpy array, where each column is patients data
+    flat_nii_files = np.column_stack(flat_nii_files)
+    # normalize if we are dealing with patients data
+    if not mask:
+        normalized_flat_nii_files = flat_nii_files / np.sum(flat_nii_files, axis=0)
+    # no need to normalize if we are dealing with mask
+    return normalized_flat_nii_files
+
+
+
+#--------------------------------------------------------------------------------------------------------------#
+#              Function to extract mask region names and function to get whole hemispheres as ROI              #
+#--------------------------------------------------------------------------------------------------------------#
+
+def mask_roi_titles(mask_txt_file):
+    masks_title = []
+    with open(mask_txt_file, 'r') as f:
+        for line in f:
+            parts = line.split()
+            if len(parts) > 1:
+                masks_title.append(parts[1])
+    masks_title_all = masks_title[:-1]               # all roi names
+    masks_title_L = masks_title[:-1:2]               # left side roi names
+    masks_title_R = masks_title[1::2]                # right side roi names
+    return masks_title_all, masks_title_L, masks_title_R
+
+
+def mask_hemispheres(mask_path, mask_txt_file):
+    mask = nib.load(mask_path)                       # loading mask
+    mask = mask.get_fdata()                          # extracting data
+    # initializing empty arrays
+    left_hemisphere_mask = np.zeros_like(mask, dtype=bool)         
+    right_hemisphere_mask = np.zeros_like(mask, dtype=bool)       
+    # iteration range is the lenght of masks_title_all (we iterate throught all rois)
+    it_range = len(mask_roi_titles(mask_txt_file)[0]) + 1
+    for i in range(1, it_range):
+        # odd regions correspond to left hemisphere
+        if i % 2 == 1:                         
+            left_hemisphere_mask |= (mask == i)      # summing together all left regions
+        # even regions correspond to right hemisphere
+        else:
+            right_hemisphere_mask |= (mask == i)     # summing together all right regions
+    # flattening the arrays
+    left_hemisphere_mask = left_hemisphere_mask.flatten()   
+    right_hemisphere_mask = right_hemisphere_mask.flatten()
+    return left_hemisphere_mask, right_hemisphere_mask
